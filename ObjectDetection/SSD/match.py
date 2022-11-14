c1389c674f8b865e20e8db83bf9f1ce45adefb1f
yudai0731 <yudai40018@gmail.com>
2022-11-14 20:20:57 +0900

    add match.py

--- ObjectDetection/SSD/match.py
+++ ObjectDetection/SSD/match.py
@@ -97,3 +97,20 @@ def match(threshold, truths, priors, variances, labels, loc_t, conf_t, idx):
     loc_t[idx] = loc
     
     conf_t[idx] = conf
+    
+def encode(matched, priors, variances):
+    """DBoxの情報[cx, cy, width, height]をDBoxのオフセット情報[Δcx, Δcy, Δw, Δh]に変換する
+
+    Args:
+        matched (Tensor): DBoxにマッチしたBBoxのアノテーションデータ
+        priors (Tensor): DBoxの情報
+        variances (list[float]): DBoxを変形するオフセット値を計算するときに使用する係数[0.1, 0.2]
+    """
+    
+    g_cxcy = (matched[:, :2] + matched[:, 2:])/2 - priors[:, :2]
+    g_cxcy /= (variances[0]*priors[:, 2:])
+    
+    g_wh = (matched[:, 2:] - matched[:, :2])/priors[:, 2:]
+    g_wh = torchlog(g_wh) / variances[1]
+    
+    return torch.cat([g_cxcy, g_wh], 1)
